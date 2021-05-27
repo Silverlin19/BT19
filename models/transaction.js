3994b2381a0816672533d4472ef67efab1ad9cd4
Silverlin19 <silverline1996@gmail.com>
2021-05-27 10:21:43 -0400

    finished?

--- models/transaction.js
+++ models/transaction.js
@@ -11,7 +11,7 @@ const transactionSchema = new Schema(
     },
     value: {
       type: Number,
-      required: "Enter an amount"
+      required: "Amount"
     },
     date: {
       type: Date,
